296b9850ab0916ecfb703e61d45eba98886bfa9e
Angels <Angels@DELL-ANGELS>
2020-11-06 20:30:38 +0100

    Versio 1

--- src/m03/uf5/alquilervehiculos/grupc/vistas/NuevoAlquilerController.java
+++ src/m03/uf5/alquilervehiculos/grupc/vistas/NuevoAlquilerController.java
@@ -5,9 +5,16 @@
  */
 package m03.uf5.alquilervehiculos.grupc.vistas;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import m03.uf5.alquilervehiculos.grupc.GestorEscenas;
 
 /**
  * FXML Controller class
@@ -16,6 +23,19 @@ import javafx.fxml.Initializable;
  */
 public class NuevoAlquilerController implements Initializable {
 
+    @FXML
+    private Button btnReservar;
+    @FXML
+    private Button btnVolver;
+    @FXML
+    private DatePicker dpFin;
+    @FXML
+    private DatePicker dpInicio;
+    @FXML
+    private ChoiceBox<?> cbNif;
+    @FXML
+    private ChoiceBox<?> cbMatricula;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +43,14 @@ public class NuevoAlquilerController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void handleBotonReservar(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleBotonVolver(ActionEvent event) throws IOException {
+        GestorEscenas.getGestor().muestraMenuPrincipal();
+    }
     
 }
