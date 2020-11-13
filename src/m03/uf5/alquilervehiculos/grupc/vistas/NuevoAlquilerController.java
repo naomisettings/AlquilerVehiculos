5fc969e3cfa5a1394a93789011fd084036eb3d77
Raimon <sellares.feiner.raimon@alumnat.copernic.cat>
2020-11-13 16:14:17 +0100

    gestor escenas actualizar nuevoalquiler

--- src/m03/uf5/alquilervehiculos/grupc/vistas/NuevoAlquilerController.java
+++ src/m03/uf5/alquilervehiculos/grupc/vistas/NuevoAlquilerController.java
@@ -179,11 +179,11 @@ public class NuevoAlquilerController implements Initializable, MiControlador {
 
     @FXML
     private void handleBotonReservar(ActionEvent event) throws IOException {
-        GestorEscenas.getGestor().muestraFactura();
         //obtenerDias();
         Alquiler a = new Alquiler(cbxNif.getValue(), cbxMatricula.getValue(),
                 dpFin.getValue().toString(), dpInicio.getValue().toString());
         Modelo.getModelo().addAlquiler(a);
+        GestorEscenas.getGestor().muestraFactura();
 
     }
 
